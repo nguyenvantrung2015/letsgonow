bc466e6351c70ef3c203b4fcfdf468af641471ea
trung <nguyenvantrung2015@gmail.com>
2017-11-17 17:09:09 +0700

    create new post

--- js/homepage.js
+++ js/homepage.js
@@ -31,4 +31,16 @@ $(document).ready(function(){
                 }
             }
     });
+    document.getElementById('getval').addEventListener('change', readURL, true);
+    function readURL(){
+    var file = document.getElementById("getval").files[0];
+    var reader = new FileReader();
+    reader.onloadend = function(){
+        document.getElementById('clock').style.backgroundImage = "url(" + reader.result + ")";        
+    }
+    if(file){
+        reader.readAsDataURL(file);
+    }else{
+    }
+}
 });
