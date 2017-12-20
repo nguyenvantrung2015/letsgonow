6a7c5386b79e1f592c0f3c289771a2bbfca8c982
hngda01 <hngda01@gmail.com>
2017-12-20 17:16:03 +0700

    Revert "Revert "messages""

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,7 @@
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
+//= require jquery.turbolinks
+//= require chosen-jquery
+//= require messages
 //= require_tree .
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212084850) do
+ActiveRecord::Schema.define(version: 20171219035720) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comment"
@@ -37,6 +37,60 @@ ActiveRecord::Schema.define(version: 20171212084850) do
     t.index ["micropost_id"], name: "index_likes_on_micropost_id"
     t.index ["user_id", "created_at"], name: "index_likes_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
+    t.string "unsubscriber_type"
+    t.integer "unsubscriber_id"
+    t.integer "conversation_id"
+    t.index ["conversation_id"], name: "index_mailboxer_conversation_opt_outs_on_conversation_id"
+    t.index ["unsubscriber_id", "unsubscriber_type"], name: "index_mailboxer_conversation_opt_outs_on_unsubscriber_id_type"
+  end
+
+  create_table "mailboxer_conversations", force: :cascade do |t|
+    t.string "subject", default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mailboxer_notifications", force: :cascade do |t|
+    t.string "type"
+    t.text "body"
+    t.string "subject", default: ""
+    t.string "sender_type"
+    t.integer "sender_id"
+    t.integer "conversation_id"
+    t.boolean "draft", default: false
+    t.string "notification_code"
+    t.string "notified_object_type"
+    t.integer "notified_object_id"
+    t.string "attachment"
+    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false
+    t.boolean "global", default: false
+    t.datetime "expires"
+    t.index ["conversation_id"], name: "index_mailboxer_notifications_on_conversation_id"
+    t.index ["notified_object_id", "notified_object_type"], name: "index_mailboxer_notifications_on_notified_object_id_and_type"
+    t.index ["notified_object_type", "notified_object_id"], name: "mailboxer_notifications_notified_object"
+    t.index ["sender_id", "sender_type"], name: "index_mailboxer_notifications_on_sender_id_and_sender_type"
+    t.index ["type"], name: "index_mailboxer_notifications_on_type"
+  end
+
+  create_table "mailboxer_receipts", force: :cascade do |t|
+    t.string "receiver_type"
+    t.integer "receiver_id"
+    t.integer "notification_id", null: false
+    t.boolean "is_read", default: false
+    t.boolean "trashed", default: false
+    t.boolean "deleted", default: false
+    t.string "mailbox_type", limit: 25
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "is_delivered", default: false
+    t.string "delivery_method"
+    t.string "message_id"
+    t.index ["notification_id"], name: "index_mailboxer_receipts_on_notification_id"
+    t.index ["receiver_id", "receiver_type"], name: "index_mailboxer_receipts_on_receiver_id_and_receiver_type"
   end
 
   create_table "microposts", force: :cascade do |t|
@@ -86,7 +140,6 @@ ActiveRecord::Schema.define(version: 20171212084850) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "address"
     t.string "hobby"
     t.string "date_of_birth"
@@ -97,6 +150,16 @@ ActiveRecord::Schema.define(version: 20171212084850) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "picture"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
